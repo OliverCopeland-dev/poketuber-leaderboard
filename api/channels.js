bd4ca0b1b0d2439cb51b21a4b90600f74d33b578
OliverCopeland-dev <olivercopland1@gmail.com>
2025-11-26 23:11:19 -0800

    Update channels list

--- api/channels.js
+++ api/channels.js
@@ -36,17 +36,30 @@ module.exports = [
   'UCwZ6Xfh9gZbtOMlAtKHdzcg', // Shugg Nastee
   'UC4be4gPsdam0hT93Drre79A', // Java Akuma
   'UCdmoLISjrUjpGIhYOKA-UYw', // Roc Sandy
+  'UCNzKuyNGREMYkx-vBYTDkRg', // okJLUV
   'UCxIcXSu10PDY8JBNfbYqjsg', // Mimik Brew
   'UCQw-gifozSHKEMyECP8q_-A', // Opossum Bud
   'UC6PSVUj7qWYpagU_3j5z04w', // Danireon
   'UC7zlHB2JcfTJXhP4ATX6tjg', // Gem Mint Pokemon
+  'UC3aHEgofR7sW1W9Up6_8Vug'. // TCGbuyersclub
+  'UClQHyDhYq-2xH0OJ9I_Y4Mw', // GabesPC
+  'UCd4UA4MC7R_1LlEFd2Jtm7g', // TCGgrading
   'UC8FyZ-7SuPVVaoxpFOE5pvA', // TwicebakedJake
+  'UCqlnaVP0xBRUf7ZnRXjfyjw', // PTCcollectibles
+  'UCs4jBZhXo1t6pVmnnfsI1tw', // oyamastrading
   'UCcwfDOpvOnFtdtbAKW7Vs_w', // RealBreakingNate
   'UCAHHqWLedf6xRyyVByhAHMw', // UnlistedLeaf
+  'UCEBIbS_nQqmPR4rMb76PdvA', // clovrcards
   'UCLIEFtisiTt59DfJi1p-wuA', // powdr tcg
+  'UCyIM5ieflmvrSQtPy7rEmVg', // TheZeroOfTime
+  'UCDFqWkHUKWDMm590DopfCvg', // Phillips Collectables
+  'UCi2k9b2vWXAfBYhBYxb7g2Q', // GTC Cards
+  'UCCfpDNu5puUHC2g9ihgN4gA', // Witkomon
+  'UC-sN_B3xdasjlsvChW-gGiQ', // TCGCrossCollector
   'UCIEiOOZQHwjB7ydEw8AHlYw', // Roboskillz
   'UCmYjK_NeE07z9h8e8x69oKg', // Chris Py TCG
   'UCgWzAf6cLXDpr2Ki1q-s8hA', // Poke Dizz
+  'UCzPIY9Z0kRrJStr-5tbTjEQ', // maxmoefoePokemon
   'UCYMxIbPsEEHJB5aWVOIrFNQ', // Radar
   'UC_bmYK4rwKR114-dmJzg8DQ', // Ian with Ian
   'UCRN3-pBsdLQJBCR7vQjXb3w', // Jay's Corner TCG
